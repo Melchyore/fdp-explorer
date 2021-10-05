60078f2428d4445ecc3fbe0da8fffaf8d20aec0a
Oussama Benhamed <wisleur@gmail.com>
2021-10-05 16:17:09 +0200

    refactor(document): perfs and fixes

--- frontend2/src/views/Document.vue
+++ frontend2/src/views/Document.vue
@@ -2,31 +2,49 @@
   <router-view>
     <section class="h-full w-4/6 flex px-4 pb-5">
       <div class="w-full h-full p-4">
-        <p class="font-semibold text-3xl tracking-wide">{{ getTitle(document) }}</p>
+        <p v-if="parents.length > 0">
+          <template v-for="(parent, index) of parents" >
+            <a :href="parent.identifier" class="text-blue-700">{{ parent.label }}</a>
+            <span v-if="index < parents.length - 1"> > </span>
+          </template>
+        </p>
 
-        <p>{{ getDescription(document) }}</p>
+        <br />
+
+        <p class="font-semibold text-3xl tracking-wide">{{ getTitle(state.document!) }}</p>
+
+        <p>{{ getDescription(state.document!) }}</p>
 
         <br/>
 
-        <p><strong>accessRights:</strong> {{ getAccessRights(document) }}</p>
-        <p><strong>hasVersion:</strong> {{ getVersion(document) }}</p>
-        <p><strong>Language:</strong> {{ language }}</p>
-        <p><strong>License:</strong> <a class="text-blue-600" :href="license" target="_blank">{{ license }}</a></p>
-        <p><strong>metadataIssued:</strong> {{ getMetadataIssued(document) }}</p>
+        <p><strong>accessRights: </strong>{{ getAccessRights(state.document!) }}</p>
+        <p><strong>hasVersion: </strong>{{ getVersion(state.document!) }}</p>
+        <p><strong>Language: </strong>
+          <template v-if="isValidHttpUrl(state.language!)">
+            <a class="text-blue-600" :href="state.language!" target="_blank">{{ state.language }}</a>
+          </template>
+          <template v-else>
+            {{ state.language }}
+          </template>
+        </p>
+        <p><strong>License: </strong><a class="text-blue-600" :href="state.license!" target="_blank">{{ state.license }}</a></p>
+        <p><strong>metadataIssued: </strong>{{ getMetadataIssued(state.document!) }}</p>
       </div>
     </section>
 
-    <card
-      v-for="(catalog, index) in catalogs"
-      :key="index"
-      :document="catalog"
-      :types="{}"
-    />
+    <template v-if="state.catalogs.length > 0">
+      <card
+        v-for="(catalog, index) in state.catalogs"
+        :key="index"
+        :document="catalog"
+        :types="{}"
+      />
+    </template>
   </router-view>
 </template>
 
-<script lang="ts">
-  import { defineComponent, onMounted, reactive, toRefs } from 'vue'
+<script setup lang="ts">
+  import { defineComponent, onMounted, reactive, ref } from 'vue'
   import { useRoute } from 'vue-router'
 
   import type { Bindings } from '@comunica/types'
@@ -34,79 +52,89 @@
   import Card from '../components/Card.vue'
 
   import executeSPARQLQuery from '../lib/SPARQL'
+  import { isValidHttpUrl } from '../lib/HTTP'
+
   import {
     getTitle,
     getDescription,
     getIdentifier,
     getCatalogs,
-    getParent,
+    getIsPartOf,
     getAccessRights,
     getVersion,
     getMetadataIssued,
     getLanguage,
-    getLicense
+    getLicense,
+    getLabel,
+    isDirectContainer
   } from '../composables/useTripleStore'
 
-  export default defineComponent({
-    name: 'Document',
+  const $route = useRoute()
+  let parents = ref<Array<Record<string, string>>>([])
 
-    components: {
-      Card
-    },
+  const state = reactive({
+    document: [] as Array<Bindings> | null,
+    catalogs: [] as Array<any>,
+    language: '' as string | null | undefined,
+    license: '' as string | null | undefined
+  })
 
-    setup () {
-      const $route = useRoute()
-      const params = $route.params
+  onMounted(async () => {
+    state.document = await executeSPARQLQuery(
+      // @ts-ignore
+      $route.params.id,
+      'SELECT ?s ?p ?o WHERE { ?s ?p ?o }'
+    )
 
-      const state = reactive({
-        document: [] as Array<Bindings> | null,
-        catalogs: [] as Array<any>,
-        language: '' as string | null | undefined,
-        license: '' as string | null | undefined
-      })
+    if (state.document) {
+      const getAllParents = async (document: Array<Bindings>) => {
+        const isPartOf = getIsPartOf(document)
+      
+        if (isPartOf) {
+          const parent = await executeSPARQLQuery(
+            // @ts-ignore
+            isPartOf,
+            'SELECT ?s ?p ?o WHERE { ?s ?p ?o }'
+          )
 
-      onMounted(async () => {
-        state.document = await executeSPARQLQuery(
-          // @ts-ignore
-          Array.from($route.params.documents).pop(),
-          'SELECT ?s ?p ?o WHERE { ?s ?p ?o }'
-        )
-
-        if (state.document) {
-          const catalogs = getCatalogs(state.document)
-          console.log(getParent(state.document))
-
-          state.language = await getLanguage(state.document)
-          state.license = await getLicense(state.document)
-
-          if (catalogs.length > 0) {
-            catalogs.forEach(async (url) => {
-              const catalog = await executeSPARQLQuery(
-                url,
-                'SELECT ?s ?p ?o WHERE { ?s ?p ?o }'
-              )
-
-              if (catalog) {
-                state.catalogs.push({
-                  title: getTitle(catalog),
-                  description: getDescription(catalog),
-                  identifier: getIdentifier(catalog),
-                  types: []
-                })
-              }
+          if (parent) {
+            parents.value.unshift({
+              identifier: getIdentifier(parent)!,
+              label: getLabel(parent)!
             })
+
+            getAllParents(parent)
           }
         }
-      })
+      }
 
-      return {
-        ...toRefs(state),
-        getTitle,
-        getDescription,
-        getAccessRights,
-        getVersion,
-        getMetadataIssued,
-        getLanguage
+      getAllParents(state.document)
+
+      state.language = await getLanguage(state.document)
+      state.license = await getLicense(state.document)
+
+      const directContainer = isDirectContainer(state.document)
+
+      if (directContainer) {
+        const catalogs = getCatalogs(state.document)
+
+        if (catalogs.length > 0) {
+          catalogs.forEach(async (url) => {
+            const catalog = await executeSPARQLQuery(
+              url,
+              'SELECT ?s ?p ?o WHERE { ?s ?p ?o }'
+            )
+
+            if (catalog) {
+              state.catalogs.push({
+                title: getTitle(catalog),
+                description: getDescription(catalog),
+                identifier: getIdentifier(catalog),
+                types: []
+              })
+            }
+          })
+        }
       }
     }
   })
